cf5f306cb4780216233020419d4d7d387cbbc681
zhoulipeng <zhoulpg@gmail.com>
2015-09-30 15:39:14 +0800

    增加filelog目录， 存放自己写的文件日志代码

---
+++ filelog/file_log.c
@@ -0,0 +1,211 @@
+/*
+ * file_log.c
+ *
+ *  Created on: Jul 8, 2015
+ *      Author: zhoulp
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "file_log.h"
+
+static FILE *file = NULL;
+static int out_priority = 0;
+static int write_count = 0;
+static int flag_fflush = 0;
+static unsigned int file_max_sizes = 0;
+//static unsigned int file_max_lines = 0;
+static pthread_mutex_t log_mtx = PTHREAD_MUTEX_INITIALIZER;
+static unsigned int threshold_switch = 10000;
+static unsigned int threshold_switch_size = 1024*1024;//default 1MB
+static unsigned int total_num = 0;
+char file_name[16][64];
+//static unsigned int cur_file_size = 0;
+static char prefix[64] = { 0 };
+
+
+char LEVEL[6][16] = {
+		{"LOG_FATAL"},
+		{"LOG_ERROR"},
+		{"LOG_WARN "},
+		{"LOG_INFO "},
+		{"LOG_DEBUG"},
+		{""}};
+
+static void Log_switch()
+{
+	printf("Log_switch \n");
+	struct stat stat_info;
+
+	sprintf(file_name[0], "%s.%d", prefix, 0);
+	stat(file_name[0], &stat_info);
+	printf("%s: %ld*%ld=%ld, %ld\n", file_name[0],
+			stat_info.st_blksize, stat_info.st_blocks, stat_info.st_blksize * stat_info.st_blocks, stat_info.st_size);
+	if(stat_info.st_size > threshold_switch_size)
+	{
+		if(file)
+		{
+			fclose(file);
+			file = NULL;
+		}
+
+		int i = total_num-1;
+		for(;i  >= 1; i--){
+			if(i == total_num -1){
+				unlink(file_name[i]);
+			}
+			printf("file_name[%d] = %s , file_name[%d] = %s\n", i-1, file_name[i-1], i, file_name[i]);
+			rename(file_name[i -1], file_name[i]);
+		}
+		file = fopen(file_name[0], "w+");
+
+	}
+
+}
+static void Log_switch_bak()
+{
+	printf("Log_switch_bak \n");
+
+
+	int i = total_num-1;
+	for(;i  >= 1; i--){
+		if(i == total_num -1){
+			unlink(file_name[i]);
+		}
+		printf("file_name[%d] = %s , file_name[%d] = %s\n", i-1, file_name[i-1], i, file_name[i]);
+		rename(file_name[i -1], file_name[i]);
+	}
+
+
+}
+int Log_init(const char *prefix_str, unsigned int log_file_num, unsigned int max_size, int priority, int buffer_flag)
+{
+	if(prefix == NULL || log_file_num < 1 || log_file_num > 16)
+	{
+		return -1;
+	}
+	if(buffer_flag){
+		flag_fflush = 0;
+	}else
+	{
+		flag_fflush = 1;
+	}
+	if(max_size < threshold_switch_size)
+		file_max_sizes = threshold_switch_size;
+		//file_max_lines = max_lines;
+	else {
+		file_max_sizes = max_size;
+		threshold_switch_size = max_size;
+		//file_max_lines = max_lines;
+	}
+
+	pthread_mutex_lock(&log_mtx);
+	memset(file_name, 0x00, sizeof(file_name));
+
+	write_count = 0;
+	total_num = log_file_num;
+	out_priority = priority;
+	strcpy(prefix, prefix_str);
+	int i = total_num-1;
+	for(;i  >= 0; i--){
+		sprintf(file_name[i], "%s.%d", prefix, i);
+		printf("file_name[%d] = %s\n", i, file_name[i]);
+	}
+	Log_switch_bak();
+	file = fopen(file_name[0], "w+");
+	pthread_mutex_unlock(&log_mtx);
+	return 0;
+}
+
+void Log_close()
+{
+	out_priority = 0;
+	if(file)
+	{
+		fclose(file);
+		file = NULL;
+	}
+
+}
+
+
+inline void Log_write(int priority, const char* a_format,...)
+{
+	if(priority > out_priority || file == NULL) return;
+	va_list va;
+	va_start(va, a_format);
+	//printf("Log_write1 \n");
+	pthread_mutex_lock(&log_mtx);
+	write_count++;
+	if(write_count % threshold_switch == 0)
+	{
+		write_count = 0;
+		Log_switch();
+	}
+	//printf("Log_write2 \n");
+
+	vfprintf(file, a_format, va);
+	if(flag_fflush)fflush(file);
+	pthread_mutex_unlock(&log_mtx);
+	va_end(va);
+}
+void *thread_write1(void *param)
+{
+	int count = 0;
+	do
+	{
+		LOG_ALL(LOG_INFO, "thread_write1_%d\n", count);
+		count++;
+	}while(count < 500000);
+
+	return NULL;
+}
+void *thread_write2(void *param)
+{
+	int count = 0;
+	do
+	{
+		LOG_ALL(LOG_WARN, "thread_write2_%d\n", count);
+		count++;
+	}while(count < 500000);
+
+	return NULL;
+}
+void *thread_write3(void *param)
+{
+	int count = 0;
+	do
+	{
+		LOG_ALL(LOG_ERROR, "thread_write2_%d\n", count);
+		count++;
+	}while(count < 500000);
+
+	return NULL;
+}
+int main1()
+{
+	Log_init("log_test", 3, 1024*1024*10, LOG_INFO, LOG_WITH_BUFFER);
+	pthread_t pid1, pid2;
+	struct timeval tv, tv_bak;
+	struct timezone tz;
+	gettimeofday (&tv_bak , &tz);
+	Log_write(1, "main start:%ld %ld\n", tv_bak.tv_sec, tv_bak.tv_usec);
+	pthread_create(&pid1, NULL, thread_write1, NULL);
+	pthread_create(&pid2, NULL, thread_write2, NULL);
+	pthread_join(pid1, NULL);
+	pthread_join(pid2, NULL);
+	gettimeofday(&tv , &tz);
+	printf("main exit:%ld %ld \n", tv.tv_sec - tv_bak.tv_sec, tv.tv_usec - tv_bak.tv_usec);
+	struct stat stat_info;
+	Log_close();
+	char file_name[64] = { 0 };
+	sprintf(file_name, "%s.%d", prefix, 0);
+	stat(file_name, &stat_info);
+	printf("%s: %ld\n", file_name, stat_info.st_blksize * stat_info.st_blocks);
+	return 0;
+}
---
+++ filelog/file_log.h
@@ -0,0 +1,65 @@
+#ifndef INCLUDE_FILE_LOG_H_
+#define INCLUDE_FILE_LOG_H_
+
+enum
+{
+	LOG_FATAL,
+	LOG_ERROR,
+	LOG_WARN,
+	LOG_INFO,
+	LOG_DEBUG,
+
+};
+enum
+{
+	LOG_WITHOUT_BUFFER,
+	LOG_WITH_BUFFER,
+
+};
+extern char LEVEL[6][16];
+
+/*
+ * prefix_str 日志文件名前缀字符串， 比如 prefix_str = "nihao_log",生成的日志文件名为 nihao_log.0
+ * log_file_num 日志文件个数， 超过这个数,日志自动覆盖， 覆盖规则
+ * max_size 日志文件大小，单位字节， 约等于这么大，如果日志文件超出指定大小，则删除最老的日志（如果存在），备份比较新的日志(nihao_log.0 改名为 nihao_log.1)，最新的日志继续写到nihao_log.0
+ * priority 日志最大打印级别 LOG_FATAL=0 LOG_ERROR=1 LOG_WARN=2 LOG_INFO=3 LOG_DEBUG=4
+ * buffer_flag 写日志是否带缓冲， 1 带缓冲 （写日志效率高， 但是可能写入不及时，不方便调试，建议发布版本使用）0 无缓冲（写入及时， 方便崩溃调试，但是写日志效率慢）
+ */
+int Log_init(const char *prefix_str, unsigned int log_file_num, unsigned int max_size, int priority, int buffer_flag);
+
+void Log_close();
+
+inline void Log_write(int priority, const char* a_format,...);
+
+
+#define LOG(level, format, ...) \
+    do { \
+    	Log_write(level, "[%s|| @ , ] " format "", \
+    			LEVEL[level], ##__VA_ARGS__ ); \
+    }while(0)
+
+#define LOG_LINE(level, format, ...) \
+    do { \
+    	Log_write(level, "[%s||@%s,%d] " format "", \
+    			LEVEL[level], __FILE__, __LINE__, ##__VA_ARGS__ ); \
+    }while(0)
+// 打印日志级别信息 + 时间 +    + 文件名 + 行数
+#define LOG_TIME(level, format, ...) \
+    do { time_t t;time(&t);struct tm tm_t; localtime_r(&t, &tm_t);\
+    	Log_write(level, "[%s||%ld|@%s,%d] " format "", \
+    			LEVEL[level], localtimetime(NULL), __FILE__, __LINE__, ##__VA_ARGS__ ); \
+    }while(0)
+// 打印日志级别信息 + 时间 +    + 文件名 + 行数
+#define LOG_TFL(level, format, ...) \
+    do { \
+    	Log_write(level, "[%s|%ld|@%s,%d] " format "", \
+    			LEVEL[level], time(NULL), __FILE__, __LINE__, ##__VA_ARGS__ ); \
+    }while(0)
+// 打印日志级别信息 + 时间 + 函数 + 文件名 + 行数
+#define LOG_ALL(level, format, ...) \
+    do { \
+    	Log_write(level, "[%s|%ld|%s@%s,%d] " format "", \
+    			LEVEL[level], time(NULL), __func__, __FILE__, __LINE__, ##__VA_ARGS__ ); \
+    }while(0)
+
+#endif /* INCLUDE_FILE_LOG_H_ */
