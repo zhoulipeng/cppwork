0b73daf81cafeb8518da13794c541ceae11f1aa8
zhoulipeng <zhoulpg@gmail.com>
2015-10-23 10:36:56 +0800

    bloglist存放学习资料及优秀博客地址libevent-book存放学习lib-event的源码

---
+++ libevent-book/01_intro/simple_httpclient.c
@@ -0,0 +1,92 @@
+/* For sockaddr_in */
+#include <netinet/in.h>
+/* For socket functions */
+#include <sys/socket.h>
+/* For gethostbyname */
+#include <netdb.h>
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+/*
+	1, http 请求头改成 "GET / HTTP/1.1" recv 函数会阻塞
+
+*/
+int main(int c, char **v)
+{
+    //const char query[] =
+    char query[128] = { 0 };
+    sprintf(query, "GET / HTTP/1.0\r\n"
+        "Host: %s\r\n"
+        "\r\n", v[1]);
+    //const char hostname[] = "www.google.com";
+    const char *hostname = v[1];
+    printf("hostname = %s\n", hostname);	
+    struct sockaddr_in sin;
+    struct hostent *h;
+    const char *cp;
+    int fd;
+    ssize_t n_written, remaining;
+    char buf[1024];
+
+    /* Look up the IP address for the hostname.   Watch out; this isn't
+       threadsafe on most platforms. */
+    h = gethostbyname(hostname);
+    if (!h) {
+        fprintf(stderr, "Couldn't lookup %s: %s", hostname, hstrerror(h_errno));
+        return 1;
+    }
+    if (h->h_addrtype != AF_INET) {
+        fprintf(stderr, "No ipv6 support, sorry.");
+        return 1;
+    }
+
+    /* Allocate a new socket */
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    /* Connect to the remote host. */
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(80);
+    sin.sin_addr = *(struct in_addr*)h->h_addr;
+    if (connect(fd, (struct sockaddr*) &sin, sizeof(sin))) {
+        perror("connect");
+        close(fd);
+        return 1;
+    }
+
+    /* Write the query. */
+    /* XXX Can send succeed partially? */
+    cp = query;
+    remaining = strlen(query);
+    while (remaining) {
+      n_written = send(fd, cp, remaining, 0);
+      if (n_written <= 0) {
+        perror("send");
+        return 1;
+      }
+      remaining -= n_written;
+      cp += n_written;
+    }
+
+    /* Get an answer back. */
+    while (1) {
+        ssize_t result = recv(fd, buf, sizeof(buf), 0);
+        if (result == 0) {
+            printf("recv result = 0\n");
+            break;
+        } else if (result < 0) {
+            perror("recv");
+            close(fd);
+            return 1;
+        }
+        fwrite(buf, 1, result, stdout);
+    }
+
+    close(fd);
+    return 0;
+}
