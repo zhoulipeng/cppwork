f85f07900abcd477e5ac39eb845fa679623d31a1
zhoulipeng <zhoulpg@gmail.com>
2016-08-25 01:38:29 +0800

    查看执行程序的编译器版本详细说明

--- shellcmd/strings_grepgcc.sh
+++ shellcmd/strings_grepgcc.sh
@@ -1,4 +1,5 @@
-# 查看可执行程序的版本信息，注意windows
-# 与linux可执行程序都可以用类似的命名查版本信息
+# 查看可执行程序的编译版本信息
+# 有些程序自带输出编译信息 (./ffmpeg -buildconf)
+# 注意windows 与linux可执行程序都可以用类似的命名查版本信息
 # 但是部分可执行程序可能查看不到
 strings ./ffmpeg|grep gcc
