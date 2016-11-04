a3a9c916d7db88340e57b37e721fc36e49b0c87c
zhoulipeng <zhoulpg@gmail.com>
2016-11-04 23:07:35 +0800

    udp之 connect的用法

--- libevent-book/epoll/udpcli.c
+++ libevent-book/epoll/udpcli.c
@@ -1,3 +1,6 @@
+// http://www.cnblogs.com/lidan/archive/2012/05/04/2482877.html
+// 根据这篇博文 ，为什么
+// udp也要调用connect函数呢，为什么udp的connect是两次握手呢
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -10,25 +13,25 @@
 #define SERV_PORT 8888
 void do_cli(FILE *fp, int sockfd, struct sockaddr *pservaddr, socklen_t servlen)
 {
-int n;
-char sendline[MAXLINE], recvline[MAXLINE + 1];
-if(connect(sockfd, (struct sockaddr *)pservaddr, servlen) == -1)
-{
-perror("connect error");
-exit(1);
-}
-while(fgets(sendline, MAXLINE, fp) != NULL)
-{
-write(sockfd, sendline, strlen(sendline));
-n = read(sockfd, recvline, MAXLINE);
-if(n == -1)
-{
-perror("read error");
-exit(1);
-}
-recvline[n] = 0;
-fputs(recvline, stdout);
-}
+    int n;
+    char sendline[MAXLINE], recvline[MAXLINE + 1];
+    if(connect(sockfd, (struct sockaddr *)pservaddr, servlen) == -1)
+    {
+        perror("connect error");
+        exit(1);
+    }   
+    while(fgets(sendline, MAXLINE, fp) != NULL)
+    {
+        write(sockfd, sendline, strlen(sendline));
+        n = read(sockfd, recvline, MAXLINE);
+        if(n == -1)
+        {
+            perror("read error");
+            exit(1);
+        }
+        recvline[n] = 0;    
+        fputs(recvline, stdout);
+    }
 }
 int main(int argc, char **argv)
 {
@@ -36,18 +39,18 @@ int sockfd;
 struct sockaddr_in servaddr;
 if(argc != 2)
 {
-printf("usage: udpclient <IPaddress>\n");
-exit(1);
+    printf("usage: udpclient <IPaddress>\n");
+    exit(1);
 }
-bzero(&servaddr, sizeof(servaddr));
-servaddr.sin_family = AF_INET;
-servaddr.sin_port = htons(SERV_PORT);
-if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-{
-printf("[%s] is not a valid IPaddress\n", argv[1]);
-exit(1);
-}
-sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-do_cli(stdin, sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-return 0;
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    {
+        printf("[%s] is not a valid IPaddress\n", argv[1]);
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    do_cli(stdin, sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    return 0;
 }
