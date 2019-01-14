a9e279f6024b632067baf236c134729f355d26eb
zhoulipeng <zhoulpg@gmail.com>
2019-01-14 10:10:12 +0800

    s3fs mount ceph and s3cmd config

---
+++ shellcmd/s3fs.sh
@@ -0,0 +1,5 @@
+s3fs zhoulp mnt/ -o url=http://10.8.107.34:8080  -o endpoint=10.8.107.34 -o use_path_request_style -o passwd_file=.passwd-s3fs
+# 3fs mybucket /mnt/tong -o url=http://obs.cn-north-1.myhwclouds.com -o endpoint=cn-north-1 -o passwd_file=/mnt/key
+#  s3fs xqb-20171211 /opt/s3point -o passwd_file=./passwd -o url=http://162.3.111.182:9000/ -o use_path_request_style -o nonempty -o use_cache=/opt/s3fscache
+
+
