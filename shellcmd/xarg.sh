752e0c798d6f72158697e0f2f6720b238c05558a
zhoulipeng <zhoulpg@gmail.com>
2019-01-15 11:14:46 +0800

    find xargs

--- shellcmd/xarg.sh
+++ shellcmd/xarg.sh
@@ -1,3 +1,5 @@
 # dpkg -l 命令可以浏览所有安装的包，其中 rc 状态的包即卸载了包却保留了配置文件。
 # 如果想要完整删除所有 rc 状态的包一个一个删还是很麻烦的，所以可以使用以下命令进行清理
 dpkg -l | grep ^rc | cut -d' ' -f3 | sudo xargs dpkg --purge  
+# 查找图片，并批量重置尺寸为1024*768, 输出到 output目录
+find . -name "*.png"|xargs -I {} convert -resize 1024x768 {} output/{}
