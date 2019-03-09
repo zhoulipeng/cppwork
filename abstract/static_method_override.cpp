87049f4b7c2c757f8db901734a713f8d7ec5ce3d
zhoulipeng <zhoulpg@gmail.com>
2019-03-09 15:33:44 +0800

    test static method

---
+++ abstract/static_method_override.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct base {
+    static void foo()
+    {
+        T::bar();
+    }
+};
+
+struct derived : public base<derived> {
+    static void bar()
+    {
+        std::cout << "derived" << std::endl;
+    }
+};
+///////////////////////////////////////////////
+class Base {
+public:
+    static int a;
+    static void Fun()
+    {
+        cout << "静态成员函数可以被继承!Base" << endl;
+    }
+};
+
+int Base::a = 1000;
+
+class Derived : public Base {
+//public:
+//    static void Fun()
+//    {
+//        cout << "静态成员函数可以被overide" << endl;
+//    }
+};
+
+int main()
+{
+    derived::foo();
+    cout << "静态数据成员可以被继承!  " << Derived::a << endl;
+    // 如果没有DerivedFun 会执行Base::Fun
+    Derived::Fun();
+}
