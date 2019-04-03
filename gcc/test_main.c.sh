3f29caef36948dc13a127b320d8aaaa1cc01109c
zhoulipeng <zhoulpg@gmail.com>
2019-04-03 15:49:14 +0800

    1.test -std=c1x -std=gnu90

--- gcc/test_main.c.sh
+++ gcc/test_main.c.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-for std in c89 c99 c11 gnu89 gnu99 gnu11; do 
-  echo $std
+for std in c89 c99 c11 c1x gnu89 gnu90 gnu99 gnu11; do 
+  echo $std "-----------------"
   gcc -std=$std -o main main.c
   ./main
-  echo
+  echo ""
 done
 echo default
 gcc -o main main.c
