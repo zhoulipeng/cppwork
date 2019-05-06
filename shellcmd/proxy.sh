2214380f0d0cfa4130e2df8f224b025a69a0a865
zhoulipeng <zhoulpg@gmail.com>
2019-05-06 10:54:58 +0800

    代理配置脚步

---
+++ shellcmd/proxy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export http_proxy=http://127.0.0.1:8123
+export https_proxy=http://127.0.0.1:8123
+export ftp_proxy=http://127.0.0.1.1:8123
+export RSYNC_PROXY=127.0.0.1:8123
+export no_proxy=127.0.0.1,localhost
+
