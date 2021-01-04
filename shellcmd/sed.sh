1c42e0974738ee503338ef93c52f3d6d90855c89
zhoulipeng <zhoulpg@gmail.com>
2021-01-04 01:26:49 +0000

    replace newline with other characters
    How replace newline with other characters when use the sed command

--- shellcmd/sed.sh
+++ shellcmd/sed.sh
@@ -1,5 +1,7 @@
 # Replace space with newline character on global mode.
 sed -i "s/ /\n/g" ipslist
 sed -e "s/ /\n/g" ipslist > ipslist.txt
+# The 'sed -i "s/\n/ /g" ' command is useless, because the sed command will add a newline for every lines.
+sed ":a;N;s/\n//g;ta" ipslist
 # Replace 'example.com' with 'chat.cs.hnyongxiong.com' in file 'prosody.cfg.lua'
 sed -i "s/example.com/chat.cs.hnyongxiong.com/g" prosody.cfg.lua
