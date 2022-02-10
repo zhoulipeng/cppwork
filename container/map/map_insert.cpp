c58db3329ac15333c1ff783660d34892c0e8cb7e
zhoulipeng <zhoulpg@gmail.com>
2022-02-10 16:14:15 +0800

    对象解析说明

--- container/map/map_insert.cpp
+++ container/map/map_insert.cpp
@@ -20,7 +20,7 @@ public:
     }
 
     Sample& operator=(const Sample& s) {
-        std::cout << "operator=" << std::endl;
+        std::cout << "operator=" << _objectNo << ", from: " << s._objectNo << std::endl;
         return *this;
     }
 
@@ -36,8 +36,13 @@ int main(int argc, char* argv[]) {
     Sample sample;
     std::map<int, Sample> map;
 
+    cout << "after map:" << endl;
+    pair<int, Sample> tmp(1, sample);
+
+    cout << "after tmp:" << endl;
     //map.insert(std::make_pair(1, sample));
-    map.insert(pair<int, Sample>(1, sample));
+    //map.insert(pair<int, Sample>(1, sample));
+    map.insert(tmp);
     //map.emplace(pair<int, Sample>(1, sample)); // c++11
     //map[1] = sample;
     std::cout << "return 0" << std::endl;
@@ -69,6 +74,11 @@ Destroying object 1
 return 0
 Destroying object 3
 Destroying object 0
+0 代表 sample
+1 代表栈内存上的 pair<> 临时对象
+2 代表insert 函数形参栈内存上的pair 对象
+3 代表map 内部二叉树堆内存的pair 对象
+
 
 ----- c++98 c++03 insert by index
 Inside default constructor of object 0
