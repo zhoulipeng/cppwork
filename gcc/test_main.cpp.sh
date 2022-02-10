9851840055b54fc9b7df0f8f3f949090a914f059
zhoulipeng <zhoulpg@gmail.com>
2022-02-10 10:57:01 +0800

    CFLAGS

--- gcc/test_main.cpp.sh
+++ gcc/test_main.cpp.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+#CFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0
+echo $CFLAGS
 for std in c++98 c++03 c++0x c++11 c++14 gnu++98 gnu++11 gnu++14; do
   echo "$std :"
-  g++ -std=$std -o main main.cpp
+  # g++ -std=$std -o main main.cpp
+  g++ -std=$std $CFLAGS -o main main.cpp
   ./main
   echo
 done
 echo "default :"
-g++ -o main main.cpp
+g++ $CFLAGS -o main main.cpp
 ./main
